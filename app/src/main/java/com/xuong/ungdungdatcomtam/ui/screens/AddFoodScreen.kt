c5e76b9e734b43c3696a360b44b24d946037be82
Nguyễn Văn Vũ <vanvu101204@gmail.com>
2024-06-07 11:09:40 +0700

    fix cautions

--- app/src/main/java/com/xuong/ungdungdatcomtam/ui/screens/AddFoodScreen.kt
+++ app/src/main/java/com/xuong/ungdungdatcomtam/ui/screens/AddFoodScreen.kt
@@ -1,20 +1,18 @@
 package com.xuong.ungdungdatcomtam.ui.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
@@ -23,76 +21,69 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
 import com.xuong.ungdungdatcomtam.R
-import com.xuong.ungdungdatcomtam.ui.controllerNav.Screen
 import com.xuong.ungdungdatcomtam.ui.theme.Inter_Family
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddFoodScreen(){
+fun AddFoodScreen() {
     Scaffold(
         topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(0xFF373232),
-                    titleContentColor = Color("#ffffff".toColorInt()),
-                ),
-                title = {
-                    Row(
+            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFF373232),
+                titleContentColor = Color("#ffffff".toColorInt()),
+            ), title = {
+                Row(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .background(Color(0xFF373232), shape = RoundedCornerShape(15.dp)),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Image(
+                        painter = painterResource(R.drawable.imgsplash),
                         modifier = Modifier
-                            .padding(5.dp)
-                            .background(Color(0xFF373232), shape = RoundedCornerShape(15.dp)),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Image(
-                            painter = painterResource(R.drawable.imgsplash),
-                            modifier = Modifier
-                                .size(56.dp)
-                                .padding(4.dp),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Text(
-                            text = "Cum tứm đim",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .padding(0.dp, 16.dp)
-                                .fillMaxWidth(),
-                            color = Color.White
-                        )
-                    }
+                            .size(56.dp)
+                            .padding(4.dp),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Cum tứm đim",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(0.dp, 16.dp)
+                            .fillMaxWidth(),
+                        color = Color.White
+                    )
                 }
-            )
+            })
         },
-    ) {innerPadding->
-        GetLayoutAddFood(innerPadding)
-
+    ) {
+        GetLayoutAddFood()
     }
 }
 
 @Composable
-fun GetLayoutAddFood(innerPadding : PaddingValues = PaddingValues()) {
+fun GetLayoutAddFood() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -103,11 +94,11 @@ fun GetLayoutAddFood(innerPadding : PaddingValues = PaddingValues()) {
     ) {
 
 
-            Image(
-                painter = painterResource(id = R.drawable.img_add),
-                contentDescription = null,
-                modifier = Modifier.size(200.dp)
-            )
+        Image(
+            painter = painterResource(id = R.drawable.img_add),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
 
         Column(
             modifier = Modifier
@@ -128,7 +119,7 @@ fun GetLayoutAddFood(innerPadding : PaddingValues = PaddingValues()) {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
-                onValueChange = {  },
+                onValueChange = { },
                 label = { Text("", fontSize = 16.sp) },
                 colors = OutlinedTextFieldDefaults.colors(
                     focusedContainerColor = containerColor,
@@ -151,7 +142,7 @@ fun GetLayoutAddFood(innerPadding : PaddingValues = PaddingValues()) {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
-                onValueChange = {  },
+                onValueChange = { },
                 label = { Text("", fontSize = 16.sp) },
                 visualTransformation = PasswordVisualTransformation(),
                 colors = OutlinedTextFieldDefaults.colors(
@@ -174,7 +165,7 @@ fun GetLayoutAddFood(innerPadding : PaddingValues = PaddingValues()) {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
-                onValueChange = {  },
+                onValueChange = { },
                 label = { Text("", fontSize = 16.sp) },
                 visualTransformation = PasswordVisualTransformation(),
                 colors = OutlinedTextFieldDefaults.colors(
@@ -197,7 +188,7 @@ fun GetLayoutAddFood(innerPadding : PaddingValues = PaddingValues()) {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
-                onValueChange = {  },
+                onValueChange = { },
                 label = { Text("", fontSize = 16.sp) },
                 visualTransformation = PasswordVisualTransformation(),
                 colors = OutlinedTextFieldDefaults.colors(
--- app/src/main/java/com/xuong/ungdungdatcomtam/ui/screens/OderDetailScreen.kt
+++ app/src/main/java/com/xuong/ungdungdatcomtam/ui/screens/OderDetailScreen.kt
@@ -33,14 +33,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
 import com.xuong.ungdungdatcomtam.R
 import com.xuong.ungdungdatcomtam.ui.theme.Inter_Family
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 data class Oder(val image: Int, val name: String, val price: String,val quantity:String)
 @Preview
